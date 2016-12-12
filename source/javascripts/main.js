51bf992633a3d91c77d88de9f8d7f2a652eb0481
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-12 14:36:14 +0100

    start the second page

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,49 +1,49 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
   // Init ScrollMagic
-  var controller = new ScrollMagic.Controller();
+  // var controller = new ScrollMagic.Controller();
 
   // Scene 1 - pin the second section
-  var pinScene01 = new ScrollMagic.Scene({
-    triggerElement: '#slide01',
-    triggerHook: 0,
-    duration: '100%'
-  })
-  .setPin('#slide01 .pin-wrapper')
-  .addTo(controller)
-  ;
+  // var pinScene01 = new ScrollMagic.Scene({
+  //   triggerElement: '#slide01',
+  //   triggerHook: 0,
+  //   duration: '100%'
+  // })
+  // .setPin('#slide01 .pin-wrapper')
+  // .addTo(controller)
+  // ;
 
   // Scene 2 - pin the third section
-  var pinScene02 = new ScrollMagic.Scene({
-    triggerElement: '#slide01',
-    triggerHook: 0,
-    duration: '200%'
-  })
-  .setPin('#slide02 .pin-wrapper')
-  .addTo(controller)
-  ;
+  // var pinScene02 = new ScrollMagic.Scene({
+  //   triggerElement: '#slide01',
+  //   triggerHook: 0,
+  //   duration: '200%'
+  // })
+  // .setPin('#slide02 .pin-wrapper')
+  // .addTo(controller)
+  // ;
 
   // Scene 3 - pin the fourth section
-  var pinScene03 = new ScrollMagic.Scene({
-    triggerElement: '#slide02',
-    triggerHook: 0,
-    duration: '200%'
-  })
-  .setPin('#slide03 .pin-wrapper')
-  .addTo(controller)
-  ;
+  // var pinScene03 = new ScrollMagic.Scene({
+  //   triggerElement: '#slide02',
+  //   triggerHook: 0,
+  //   duration: '200%'
+  // })
+  // .setPin('#slide03 .pin-wrapper')
+  // .addTo(controller)
+  // ;
 
   // Scene 4 - pin the fifth section
-  var pinScene04 = new ScrollMagic.Scene({
-    triggerElement: '#slide03',
-    triggerHook: 0,
-    duration: '100%'
-  })
-  .setPin('#slide04 .pin-wrapper')
-  .addTo(controller)
-  ;
+//   var pinScene04 = new ScrollMagic.Scene({
+//     triggerElement: '#slide03',
+//     triggerHook: 0,
+//     duration: '100%'
+//   })
+//   .setPin('#slide04 .pin-wrapper')
+//   .addTo(controller)
+//   ;
 
-});
+// });
 
 
  // triggerhook: 0 => start at the top of the viewport
