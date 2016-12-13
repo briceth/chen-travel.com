6cc581d332bcbbd073e6882dabfece23f1bd210e
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-13 20:26:20 +0100

    add some animation the padlock view

--- source/javascripts/scrolling-icones.js
+++ source/javascripts/scrolling-icones.js
@@ -4,8 +4,13 @@ $(document).ready(function(){
 // hide our element on page load
 // $('.icones').css('opacity', '0');
 
-$('.icones').waypoint(function() {
-  $('.icones').addClass('bounceInLeft');
-}, { offset: '100%' });
+  $('.icones').waypoint(function() {
+    $('.icones').addClass('bounceInLeft');
+    $('.edition').css('opacity', '0');
+    $('.edition').delay(600).queue(function () {
+        $('.edition').addClass('bounceInRight');
+        $('.edition').css('opacity', '1');
+    });
+  }, { offset: '100%' });
 
 });
