badea084706d51b30662e9d504e2776ca05207c3
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-14 19:38:30 +0100

    add some fucking good animation

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
 
+  $('.class').css('opacity', '0');
+  $('.second_page img').css('opacity', '0');
+
+  $('.second_page').waypoint(function() {
+
+    $('.second_page img').css('opacity', '1');
+    $('.second_page img:eq(0)').attr('id', 'weight');
+    $('.second_page img:eq(1)').attr('id', 'tracking');
+    $('.second_page img:eq(2)').attr('id', 'padlock');
+    $('.second_page img:eq(3)').attr('id', 'battery');
+
+    $('.class').delay(3000).queue(function () {
+            $(this).addClass('features-icones');
+            $(this).css('opacity', '1');
+        });
+
+  }, { offset: '80%' });
+
+});
