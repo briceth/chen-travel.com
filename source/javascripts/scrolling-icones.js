639444e077847711d5e045efef955ac1b0503a60
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-15 10:37:01 +0100

    manage the beug

--- source/javascripts/scrolling-icones.js
+++ source/javascripts/scrolling-icones.js
@@ -3,15 +3,20 @@ $(document).ready(function(){
 
 // hide our element on page load
   $('.icones').css('opacity', '0');
+  $('.edition').css('opacity', '0');
+  $('.button-order').css('opacity', '0');
 
   $('.icones').waypoint(function() {
     $('.icones').addClass('bounceInLeft');
     $('.icones').css('opacity', '1');
-    $('.edition').css('opacity', '0');
     $('.edition').delay(600).queue(function () {
         $(this).addClass('bounceInRight');
         $(this).css('opacity', '1');
+        $('.button-order').delay(2500).queue(function() {
+          $(this).addClass('fadeInUp');
+          $(this).css('opacity', '1');
+        });
     });
-  }, { offset: '70%' });
+  }, { offset: '80%' });
 
 });
