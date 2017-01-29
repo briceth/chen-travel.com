5ce936b737206b269bab270958009958c0f31bd2
Brice Tessier Huort <tessierhuort@gmail.com>
2017-01-29 16:18:59 +0100

    change the page 2

--- source/javascripts/scrolling-icones.js
+++ source/javascripts/scrolling-icones.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-// hide our element on page load
-  $('.icones').css('opacity', '0');
-  $('.edition').css('opacity', '0');
-  $('.button-order').css('opacity', '0');
+// // hide our element on page load
+//   $('.icones').css('opacity', '0');
+//   $('.edition').css('opacity', '0');
+//   $('.button-order').css('opacity', '0');
 
-  $('.icones').waypoint(function() {
-    $('.icones').addClass('bounceInLeft');
-    $('.icones').css('opacity', '1');
-    $('.edition').delay(600).queue(function () {
-        $(this).addClass('bounceInRight');
-        $(this).css('opacity', '1');
-        $('.button-order').delay(2500).queue(function() {
-          $(this).addClass('fadeInUp');
-          $(this).css('opacity', '1');
-        });
-    });
-  }, { offset: '80%' });
+//   $('.icones').waypoint(function() {
+//     $('.icones').addClass('bounceInLeft');
+//     $('.icones').css('opacity', '1');
+//     $('.edition').delay(600).queue(function () {
+//         $(this).addClass('bounceInRight');
+//         $(this).css('opacity', '1');
+//         $('.button-order').delay(2500).queue(function() {
+//           $(this).addClass('fadeInUp');
+//           $(this).css('opacity', '1');
+//         });
+//     });
+//   }, { offset: '80%' });
 
-});
+// });
