75d4eec9cd38d7e29f8e024952a99b216184c3a2
Brice Tessier Huort <tessierhuort@gmail.com>
2017-02-06 12:56:07 +0100

    make some order

--- source/javascripts/scrolling-nav.js
+++ source/javascripts/scrolling-nav.js
@@ -1,27 +1,27 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 
-$(document).ready(($) => {
-    $('.scroll').bind('click.scroll', (e) => {
-        e.preventDefault();
+// $(document).ready(($) => {
+//     $('.scroll').bind('click.scroll', (e) => {
+//         e.preventDefault();
 
-        // Get the current target hash
-        var target = this.hash;
-        // console.log(target);
+//         // Get the current target hash
+//         var target = this.hash;
+//         // console.log(target);
 
-        // Animate the scroll bar action so its smooth instead of a hard jump
-        $('html, body').stop().animate({
-            'scrollTop' : $(target).offset().top
-        }, 1000, 'swing', () => {
-            window.location.hash = target;
-        });
-    });
-});
+//         // Animate the scroll bar action so its smooth instead of a hard jump
+//         $('html, body').stop().animate({
+//             'scrollTop' : $(target).offset().top
+//         }, 1000, 'swing', () => {
+//             window.location.hash = target;
+//         });
+//     });
+// });
 
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+// //jQuery to collapse the navbar on scroll
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
