dcae36dd25f0321f373a4c50dbee1696c59dda11
Brice Tessier Huort <tessierhuort@gmail.com>
2017-02-06 16:57:27 +0100

    picture reappear

--- source/javascripts/team.js
+++ source/javascripts/team.js
@@ -1,14 +1,14 @@
-$(document).ready(function (){
+// $(document).ready(function (){
 
-  $('.card').css('opacity', '0');
+//   //$('.card').css('opacity', '0');
 
-  $('.profile').waypoint(function() {
-    $('.board').addClass('fadeInUp');
-    $('.board').css('opacity', '1');
-      $('.employee').delay(600).queue(function () {
-        $(this).addClass('fadeInUp');
-        $(this).css('opacity', '1');
-  }, { offset: '100%' });
+//   $('.profile').waypoint(function() {
+//     $('.board').addClass('fadeInUp');
+//     $('.board').css('opacity', '1');
+//       $('.employee').delay(600).queue(function () {
+//         $(this).addClass('fadeInUp');
+//         $(this).css('opacity', '1');
+//   }, { offset: '100%' });
 
-  });
-});
+//   });
+// });
